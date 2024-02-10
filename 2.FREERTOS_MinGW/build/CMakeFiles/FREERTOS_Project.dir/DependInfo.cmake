ac06aa3d20cf1448df629f484dfa76564ec15679
l0190 <3221410461@qq.com>
2024-02-10 18:21:36 +0800

    first

---
+++ 2.FREERTOS_MinGW/build/CMakeFiles/FREERTOS_Project.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/croutine.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/croutine.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/event_groups.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/event_groups.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/list.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/list.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/portable/MSVC-MingW/port.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/portable/MSVC-MingW/port.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/portable/MemMang/heap_3.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/portable/MemMang/heap_3.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/queue.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/queue.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/stream_buffer.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/stream_buffer.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/tasks.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/tasks.c.obj"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/timers.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/timers.c.obj"
+  "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/src/main.c" "C:/Users/amin/Desktop/git/FREERTOS-Win/4.FREERTOS_VsCode_MinGW/build/CMakeFiles/FREERTOS_Project.dir/src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/include"
+  "D:/tools/code/c/FreeRTOS/FreeRTOS/portable/MSVC-MingW"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2.FREERTOS_MinGW/build/CMakeFiles/FREERTOS_Project.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/croutine.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/event_groups.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/list.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/portable/MSVC-MingW/port.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/portable/MemMang/heap_3.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/queue.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/stream_buffer.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/tasks.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/D_/tools/code/c/FreeRTOS/FreeRTOS/timers.c.obj"
+  "CMakeFiles/FREERTOS_Project.dir/src/main.c.obj"
+  "FREERTOS_Project.exe"
+  "FREERTOS_Project.exe.manifest"
+  "FREERTOS_Project.pdb"
+  "libFREERTOS_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FREERTOS_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
