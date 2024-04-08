6b68976f52f3491cbd9455f32b3f73de06870364
l0190 <3221410461@qq.com>
2024-04-08 11:11:42 +0800

    gcc

---
+++ FreeRTOS-Kernel/build/portable/CMakeFiles/freertos_kernel_port.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/amin/Downloads/FreeRTOS-LTS/FreeRTOS/FreeRTOS-Kernel/portable/MSVC-MingW/port.c" "C:/Users/amin/Downloads/FreeRTOS-LTS/FreeRTOS/FreeRTOS-Kernel/build/portable/CMakeFiles/freertos_kernel_port.dir/MSVC-MingW/port.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../portable/MSVC-MingW"
+  "../include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/amin/Downloads/FreeRTOS-LTS/FreeRTOS/FreeRTOS-Kernel/build/CMakeFiles/freertos_kernel.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FreeRTOS-Kernel/build/portable/CMakeFiles/freertos_kernel_port.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freertos_kernel_port.dir/MSVC-MingW/port.c.obj"
+  "libfreertos_kernel_port.a"
+  "libfreertos_kernel_port.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/freertos_kernel_port.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
